584502edfe14d9d57c4d77d6911af0682fa6eda9
ktubo <kira.tubo@gmail.com>
2023-05-04 20:46:20 -0700

    Add author and date complete

--- duplicateEncoder/main.py
+++ duplicateEncoder/main.py
@@ -1,9 +1,13 @@
 """
+From CodeWars:
+
 The goal of this exercise is to convert a string to a new string where each character
 in the new string is "(" if that character appears only once in the original string,
 or ")" if that character appears more than once in the original string.
 Ignore capitalization when determining if a character is a duplicate.
 
+Created by: Kira Tubo
+Completed on: Apr 20, 2023
 """
 
 
