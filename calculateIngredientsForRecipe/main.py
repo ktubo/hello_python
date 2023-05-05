0c353419590db684d6c0fbad6e1674a89fb5ab77
Kira Tubo <131325524+ktubo@users.noreply.github.com>
2023-05-04 20:58:57 -0700

    Add author and date completed

--- calculateIngredientsForRecipe/main.py
+++ calculateIngredientsForRecipe/main.py
@@ -15,6 +15,8 @@ cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, mil
 # must return 0
 cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})
 
+Completed by Kira Tubo
+Created on Apr 25, 2023
 """
 
 
